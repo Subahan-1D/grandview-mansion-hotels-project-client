051a7891b39536a7d4c2dc36fda06b85acfedc7a
subahan ali <subahanislam523@gmail.com>
2024-09-06 12:10:30 +0600

    add to become a host button function

--- src/components/Dashbord/Sidebar/Menu/GuestMenu.jsx
+++ src/components/Dashbord/Sidebar/Menu/GuestMenu.jsx
@@ -2,9 +2,46 @@ import { BsFingerprint } from "react-icons/bs";
 import { GrUserAdmin } from "react-icons/gr";
 import MenuItem from ".//MenuItem";
 import useRole from "../../../../hooks/useRole";
+import HostModal from "../../../Modal/HostRequestModal";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const GuestMenu = () => {
-  const [role] = useRole()
+  const [role] = useRole();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  // for modal
+   const [isModalOpen, setIsModalOpen] = useState(false);
+   const closeModal = async () => {
+     setIsModalOpen(false);
+   };
+
+   // continue a click or database host yes
+   const modalHandler = async () => {
+     console.log("i want to be a host");
+     // new request admin
+     try {
+       const currentUser = {
+         email: user?.email,
+         role: "guest",
+         status: "Requested",
+       };
+       const { data } = await axiosSecure.put(`/user`, currentUser);
+       console.log(data);
+       if (data.modifiedCount > 0) {
+         toast.success("Success! Please wait for admin confirmation");
+       } else {
+         toast.success("Please!, Wait for admin approval👊");
+       }
+     } catch (err) {
+       console.log(err);
+       toast.error(err.message);
+     } finally {
+       closeModal();
+     }
+   };
   return (
     <>
       <MenuItem
@@ -14,12 +51,21 @@ const GuestMenu = () => {
       />
 
       {role === "guest" && (
-        <div className="flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform text-gray-600  hover:bg-gray-300   hover:text-gray-700 cursor-pointer">
+        <div
+          onClick={() => setIsModalOpen(true)}
+          className="flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform text-gray-600  hover:bg-gray-300   hover:text-gray-700 cursor-pointer"
+        >
           <GrUserAdmin className="w-5 h-5" />
 
           <span className="mx-4 font-medium">Become A Host</span>
         </div>
       )}
+
+      <HostModal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        modalHandler={modalHandler}
+      ></HostModal>
     </>
   );
 };
